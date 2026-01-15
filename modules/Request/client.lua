27941a2658e74d79fc5f1407feb066da399718ff
Mani <71591933+manipou@users.noreply.github.com>
2026-01-15 05:11:18 +0100

    Request Streaming

---
+++ modules/Request/client.lua
@@ -0,0 +1,74 @@
+--[[
+    https://github.com/overextended/ox_lib
+
+    This file is licensed under LGPL-3.0 or higher <https://www.gnu.org/licenses/lgpl-3.0.en.html>
+
+    Copyright © 2025 Linden <https://github.com/thelindat>
+]]
+
+local module = {}
+
+---@async
+---@generic T : string | number
+---@param request function
+---@param hasLoaded function
+---@param assetType string
+---@param asset T
+---@param timeout? number
+local function streamingRequest(request, hasLoaded, assetType, asset, timeout)
+    if hasLoaded(asset) then return asset end
+
+    request(asset)
+
+    return jet.Util.waitFor(function()
+            if hasLoaded(asset) then return asset end
+        end, ("failed to load %s '%s' - this may be caused by\n- too many loaded assets\n- oversized, invalid, or corrupted assets"):format(assetType, asset),
+        timeout or 30000)
+end
+
+---@param model number | string
+---@param timeout number? Approximate milliseconds to wait for the model to load. Default is 10000.
+---@return number model
+function module.model(model, timeout)
+    if type(model) ~= 'number' then model = joaat(model) end
+    if HasModelLoaded(model) then return model end
+
+    if not IsModelValid(model) and not IsModelInCdimage(model) then
+        error(("attempted to load invalid model '%s'"):format(model))
+    end
+
+    return streamingRequest(RequestModel, HasModelLoaded, 'model', model, timeout)
+end
+
+---@param animDict string
+---@param timeout number? Approximate milliseconds to wait for the dictionary to load. Default is 10000.
+function module.animDict(animDict, timeout)
+    if HasAnimDictLoaded(animDict) then return animDict end
+
+    if type(animDict) ~= 'string' then
+        error(("expected animDict to have type 'string' (received %s)"):format(type(animDict)))
+    end
+
+    if not DoesAnimDictExist(animDict) then
+        error(("attempted to load invalid animDict '%s'"):format(animDict))
+    end
+
+    return streamingRequest(RequestAnimDict, HasAnimDictLoaded, 'animDict', animDict, timeout)
+end
+
+---@param ptFxName string
+---@param timeout number? Approximate milliseconds to wait for the particle effect to load. Default is 10000.
+---@return string ptFxName
+function module.namedPtfxAsset(ptFxName, timeout)
+    if HasNamedPtfxAssetLoaded(ptFxName) then return ptFxName end
+
+    if type(ptFxName) ~= 'string' then
+        error(("expected ptFxName to have type 'string' (received %s)"):format(type(ptFxName)))
+    end
+
+    return streamingRequest(RequestNamedPtfxAsset, HasNamedPtfxAssetLoaded, 'ptFxName', ptFxName, timeout)
+end
+
+-- TODO: DOESNT WORK YET
+
+return module
