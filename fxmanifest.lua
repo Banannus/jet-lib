a6edc0b54800150cf7ddc5076ce0881ca440a687
Banannus <jakobvagtborg@stofanet.dk>
2026-01-15 06:20:39 +0100

    fxmanifest fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,10 @@ Author 'bndev & manimods'
 Description 'Library for bndev and manimods resources'
 Version '1.0.0'
 
+shared_scripts {
+    '@ox_lib/init.lua',
+} 
+
 client_scripts {
     'client.lua',
 }
@@ -14,16 +18,11 @@ server_scripts {
 }
 
 files {
+    'init.lua',
     'modules/**/*.lua',
     'modules/**/*.lua',
     'modules/**/*.lua',
 }
-
-shared_scripts {
-    'dependencies.lua',
-    'init.lua',
-    '@ox_lib/init.lua',
-} 
 
 lua54 'yes'
 
